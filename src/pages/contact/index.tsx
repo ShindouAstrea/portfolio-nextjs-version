8f6bd61b1de0c93fe7e9bb5b5c704a39ca9edb39
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-04-24 20:59:34 -0400

    Add: new pages

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,13 @@
+import NavBar from "@/components/NavBar.component";
+
+export default function Contact(){
+    return (
+        
+        <div> 
+            <NavBar/>
+            <div>
+                <h2>Hola</h2>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,13 @@
+import NavBar from "@/components/NavBar.component";
+
+export default function Projects(){
+    return (
+        
+        <div> 
+            <NavBar/>
+            <div>
+                <h2>Hola</h2>
+            </div>
+        </div>
+    )
+}
