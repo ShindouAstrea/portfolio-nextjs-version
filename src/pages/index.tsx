7362559a00533d633a07ddfc85785f29f8face39
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-04-24 20:59:48 -0400

    Change: refactor main function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import NavBar from "@/components/NavBar.component";
-
+import Main from "@/components/Main.component";
 export default function Home() {
   return (
     <div>
       <NavBar/>
+      <Main/>
     </div>
     )
 }
