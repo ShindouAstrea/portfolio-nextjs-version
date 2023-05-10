145c7a8a156200bf52c3ccce96b26d6aeba182c8
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-05-10 16:37:18 -0400

    ADD: code to export assets

---
+++ public/assets/icons/languages/index.ts
@@ -0,0 +1,14 @@
+import C from './c.svg';
+import Java from './java.svg';
+import Javascript from './javascript.svg';
+import Php from './php.svg';
+import Typescript from './typscript.svg';
+import Python from './python.svg';
+export default {
+  C,
+  Java,
+  Javascript,
+  Php,
+  Typescript,
+  Python
+}
---
+++ public/assets/icons/rrss/index.ts
@@ -0,0 +1,10 @@
+import Git from './github.svg';
+import Gmail from './gmail.svg';
+import Linkedin from './link.svg';
+import Twitter from './twt.svg';
+export default {
+    Git,
+    Gmail,
+    Linkedin,
+    Twitter,
+}
