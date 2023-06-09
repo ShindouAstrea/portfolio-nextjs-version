c7a9e08cc43f81ee9c2ecef2c4be641ffc62168e
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-06-09 09:27:42 -0400

    Fix: responsive design

--- src/components/Contact.component.tsx
+++ src/components/Contact.component.tsx
@@ -4,15 +4,15 @@ export default function ContactMain(){
     const iconsArray = Object.entries(Icons).map(([key, value]) => ({ key, value }));
     return (
         <div className="w-full lg:h-screen p-2">
-        <div className=" max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-            <p> Contacto</p>
-            <h2>Como puedes Contactarme?</h2>
+        <div className=" max-w-[1240px] mx-auto flex flex-col justify-center h-full mt-[8rem] text-center">
+            <h1 className="uppercase text-4xl font-bold"> Contacto</h1>
+            <p className="my-4 p-5 text-lg">Contáctame por medio de mis redes sociales:</p>
                 <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
                 {iconsArray.map((icon)=>
                   <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-100 bg-[#0e122a]">
                       <div className='flex flex-col items-center justify-center text-center '>
-                        <div className='md:max-w-[100px]-max-h-[100px] lg:max-w-[250px]-max-h-[250px]'>
-                          <Image  key={icon.key} src={icon.value} alt={icon.key} />
+                        <div >
+                          <Image  key={icon.key} src={icon.value} alt={icon.key} className="min-[300px]:w-[80px] sm:w-[100px] lg:w-[200px]"/>
                           <h3>{icon.key}</h3>
                         </div>
                       </div>
