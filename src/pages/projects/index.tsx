765fb337b1df4f5bbc7357f889309220c37ed81b
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-06-09 09:24:37 -0400

    Change: Add project component

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,11 +1,12 @@
 import NavBar from "@/components/NavBar.component";
+import ProjectsMain from "@/components/Projects.component";
 
 export default function Projects(){
     return (
         
         <div> 
             <NavBar/>
-           
+            <ProjectsMain/>
         </div>
             
     )
