b9a1947e830073c9e0d722c6449ea8a8fcde8d5b
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-06-09 09:48:58 -0400

    Fix: tipography

--- src/components/SkillsMain.component.tsx
+++ src/components/SkillsMain.component.tsx
@@ -7,9 +7,9 @@ export default function SkillsMain(){
 
     return(
         <div className="w-full lg:h-screen p-2">
-            <div className=" max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-                <p> Habilidades</p>
-                <h2>Con qué he trabajado?</h2>
+            <div className=" max-w-[1240px] mx-auto flex flex-col justify-center h-full text-center">
+                <h1 className='uppercase text-4xl font-bold'> Habilidades</h1>
+                <p className='my-4 p-5 text-lg '>Con qué he trabajado?</p>
                     <div className="grid md:grid-cols-2 my-5 lg:grid-cols-4 gap-8">
                     {iconsArray.map((icon)=>
                       <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-100 bg-[#0e122a]">
