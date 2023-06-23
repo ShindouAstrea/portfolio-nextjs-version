46b65d54e8eb75192df2e09367bfdaf7786f06e1
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-06-22 22:02:39 -0400

    Fix:  RRSS link to their profiles

--- src/components/Contact.component.tsx
+++ src/components/Contact.component.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import Image from "next/image";
 import Icons from '../../public/assets/icons/rrss/';
+function bindLink(key: string){
+  console.log(key);
+  switch(key){
+    case 'Gmail':
+      return 'mailto:jose.silva.a@mail.pucv.cl'
+      
+    case 'Git':
+      return 'https://github.com/ShindouAstrea';
+     
+    case 'Linkedin':
+      return 'https://www.linkedin.com/in/josesilvaaraneda/';
+      
+    case 'Twitter':
+      return 'https://twitter.com/josebastian_';
+  }
+}
 export default function ContactMain(){
     const formHandler = (event: React.MouseEvent<HTMLFormElement>)=>{
       event.preventDefault();
@@ -9,24 +25,33 @@ export default function ContactMain(){
       const formJson = Object.fromEntries(data.entries());
       console.log(formJson);
     }
-    const iconsArray = Object.entries(Icons).map(([key, value]) => ({ key, value }));
+    const iconsArray = Object.entries(Icons).map(([key, value]) => ({ key, value,link:bindLink(key) }));
     return (
         <div className="w-full lg:h-screen p-2">
-        <div className=" max-w-[1240px] mx-auto flex flex-col justify-center h-full mt-[8rem] text-center">
-          <h1 className="uppercase text-4xl font-bold"> Contacto</h1>
-          <p className="my-4 p-5 text-lg">Contáctame por medio de mis redes sociales:</p>
-            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-              {iconsArray.map((icon)=>
-                <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-100 bg-[#0e122a]" key={icon.key}>
-                  <div className='flex flex-col items-center justify-center text-center 'key={icon.key}>
-                    <div key={icon.key}>
-                      <Image  key={icon.key} src={icon.value} alt={icon.key} className="min-[300px]:w-[80px] sm:w-[100px] lg:w-[200px]"/>
-                      <h3>{icon.key}</h3>
+          <div className=" max-w-[1240px] mx-auto flex flex-col justify-center h-full mt-[8rem] text-center">
+              <h1 className="uppercase text-4xl font-bold"> Contacto</h1>
+          
+           
+            <p className="my-4 p-5 text-lg">Contáctame por medio de mis redes sociales:</p>
+              <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+                {iconsArray.map((icon,index)=>
+                <>
+                  
+                  <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-100 bg-[#0e122a]" key={`${index}-${icon.key}`}>
+
+                    <div className='flex flex-col items-center justify-center text-center cursor-pointer 'key={`${icon.key}-${index}-${index}`}>
+                    <a  key={`${icon.key}-${icon.key}`} href={icon.link} target="_blank" rel="noopener noreferrer" >
+                        <div key={icon.key}>
+                          <Image  key={icon.key} src={icon.value} alt={icon.key} className="min-[300px]:w-[80px] sm:w-[100px] lg:w-[200px]"/>
+                          <h3 key={`${icon.key}-${icon.key}-${icon.key}`}>{icon.key}</h3>
+                        </div>
+                    </a>
+                     
                     </div>
                   </div>
-                </div>
-              )}
-            </div>
+                </>
+                )}
+              </div>
           <div className="my-4 flex flex-col justify-center items-center">
             <p className="my-4 p-5 text-lg"> o puedes escrbirme directamente y cuando sea posible te respondo :</p>
             <form method="Post" aria-labelledby='Formulario de contacto' onSubmit={formHandler}>
