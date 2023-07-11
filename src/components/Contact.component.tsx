b42078fda2fef95136a01ada9e648e0bd30acc51
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-07-11 15:53:25 -0400

    Change: section form

--- src/components/Contact.component.tsx
+++ src/components/Contact.component.tsx
@@ -58,10 +58,8 @@ export default function ContactMain(){
    
     return (
         <div className="w-full lg:h-screen p-2">
-          <div className=" max-w-[1240px] mx-auto flex flex-col justify-center h-full mt-[8rem] text-center">
-              <h1 className="uppercase text-4xl font-bold"> Contacto</h1>
-          
-           
+           <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full mt-[8rem] text-center'>
+              <h1 className="uppercase text-4xl font-bold"> Contacto</h1>    
             <p className="my-4 p-5 text-lg">Contáctame por medio de mis redes sociales:</p>
               <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
                 {iconsArray.map((icon,index)=>
@@ -79,25 +77,27 @@ export default function ContactMain(){
                   </React.Fragment>
                 )}
               </div>
-          <div className="my-4 flex flex-col justify-center items-center">
-            <p className="my-4 p-5 text-lg"> o puedes escrbirme directamente y cuando sea posible te respondo :</p>
-            <form method="Post" aria-labelledby='Formulario de contacto' onSubmit={formHandler}>
-              <div className="my-4 lg:w-50 "aria-labelledby='formulario correo'>
-                <input name="email"type="email" autoComplete="FALSE" className="w-full h-14 "aria-label='entrada-correo'placeholder='Correo'/>
-              </div>
-              <div  className="my-4 lg:w-[500px]  "aria-labelledby="formulario-asunto">
-                <input name="subject"type="text"  autoComplete="TRUE"className="w-full h-14"aria-label='entrada-asunto'placeholder='Asunto'/>
-              </div>
-              <div  className="my-4 lg:w-[500px]"aria-labelledby='formulario mensaje'>
-                <textarea name="message"   rows={10} cols={10} autoComplete="FALSE"className="w-full bg-slate-800 border-b-2 border-slate-600 outline-none text-white p-6 resize-none"aria-label='entrada-mensaje'placeholder='Mensaje'/>
-              </div>
-              <div aria-labelledby="boton-envio-formulario" className='lg:w-50'>
-                <button type="submit" aria-label="Boton para enviar" className="w-full h-14 bg-[#087EA4] "> Enviar</button>
+          <section>
+            <div className="my-4 flex flex-col justify-center items-center">
+              <p className="my-4 p-5 text-lg"> o puedes escrbirme directamente y cuando sea posible te respondo :</p>
+              <form method="Post" aria-labelledby='Formulario de contacto' onSubmit={formHandler}>
+                <div className="my-4 lg:w-50 "aria-labelledby='formulario correo'>
+                  <input name="email"type="email" autoComplete="FALSE" className="w-full h-14 "aria-label='entrada-correo'placeholder='Correo'/>
+                </div>
+                <div  className="my-4 lg:w-[500px]  "aria-labelledby="formulario-asunto">
+                  <input name="subject"type="text"  autoComplete="TRUE"className="w-full h-14"aria-label='entrada-asunto'placeholder='Asunto'/>
+                </div>
+                <div  className="my-4 lg:w-[500px]"aria-labelledby='formulario mensaje'>
+                  <textarea name="message"   rows={10} cols={10} autoComplete="FALSE"className="w-full bg-slate-800 border-b-2 border-slate-600 outline-none text-white p-6 resize-none"aria-label='entrada-mensaje'placeholder='Mensaje'/>
+                </div>
+                <div aria-labelledby="boton-envio-formulario" className='lg:w-50'>
+                  <button type="submit" aria-label="Boton para enviar" className="w-full h-14 bg-[#087EA4] "> Enviar</button>
 
-              </div>
-            </form>
+                </div>
+              </form>
 
-          </div>
+            </div>
+          </section>
         </div>
     </div> 
     )
