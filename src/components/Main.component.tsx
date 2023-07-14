9c08d9b9ae7cb73771b74ec52ac476ef820c6862
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-07-13 23:53:59 -0400

    Change: Refactor render return

--- src/components/Main.component.tsx
+++ src/components/Main.component.tsx
@@ -1,29 +1,9 @@
 import React  from 'react';
 
-import Foto from '../../public/assets/pictures/dev.png';
-
-import Modal from './Modal.component';
 import Link from 'next/link';
 export default  function Main(){
-
-    // const InitialContext = {
-    //     show:false,
-    //     Xpos:0,
-    //     Ypos:0
-    // }
-    //const [contextMenu,setContextMenu] = React.useState(InitialContext);
     const handlerContext = (event: React.MouseEvent<HTMLDivElement,globalThis.MouseEvent>)=>{
         event.preventDefault();
-        const {pageX, pageY} = event;
-        //setContextMenu({show:true,Xpos:pageX, Ypos:pageY});
-
-
-    }
-    
-    const [showFoto,setShowFoto] =React.useState(true);
-    const handleFoto =()=>{
-        setShowFoto(!showFoto) ;
-        
     }
     return(
         <div  className=' w-full lg:h-screen p-2 ' onContextMenu={handlerContext}>
@@ -32,10 +12,7 @@ export default  function Main(){
                     <h1 className=' text-white text-4xl font-bold uppercase text-center min-[300px]:w-[20rem] px-2'>Acerca de mi</h1>
                    
 
-                    <h2 className='text-white text-3xl text-center justify-center items-center '> Hola !  , soy <span className='text-[#85c1e9]'  onMouseOver={handleFoto} onMouseOut={handleFoto}>José Silva </span>un Ingeniero de Software.</h2>
-                    <div className=' relative justify-center items-center z-10'>
-                        <Modal imageRoute={Foto} state={showFoto} />
-                    </div>
+                    <h2 className='text-white text-3xl text-center justify-center items-center uppercase '> Hola !  , soy <span id ="nombre"className='text-[#85c1e9] uppercase'>José Silva </span>un Ingeniero de Software.</h2>
                     <p className="text-center justify-center text-gray-400 text-2xl lg:w-[80rem] px-1 ">
                         Soy ingeniero de ejecución en informática con principal experiencia en el desarrollo web.
                         Me considero alguien adaptable y con habilidades interpersonales sólidas, lo que me permite trabajar bien en equipo y con personas de diferentes
