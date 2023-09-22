7d38f1848f50412ad571c939476ff60853df1557
Jose Silva <jose.silva.a@mail.pucv.cl>
2023-09-22 01:15:07 -0300

    Add: se agrga pagina de experiencias

---
+++ src/pages/experiences/index.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/components/NavBar.component";
+import ExperienceMain from "@/components/Experience.component";
+export default function Experiences() {
+    return (
+
+        <div>
+            <NavBar />
+            <ExperienceMain />
+        </div>
+    )
+}
