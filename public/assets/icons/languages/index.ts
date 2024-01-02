2559a66bf5056b55c6e414ca5026a67d59ee6a12
jsilva <jsilva@cdgo.cl>
2024-01-02 09:34:21 -0300

    Change: Se agrega icono de dart

--- public/assets/icons/languages/index.ts
+++ public/assets/icons/languages/index.ts
@@ -4,11 +4,13 @@ import Javascript from './javascript.svg';
 import Php from './php.svg';
 import Typescript from './typscript.svg';
 import Python from './python.svg';
+import Dart from './dart.svg'
 export default {
   C,
   Java,
   Javascript,
   Php,
   Typescript,
-  Python
+  Python,
+  Dart
 }
