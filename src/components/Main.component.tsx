e2346a400b73a52e0677a45153cbaa9a3617572d
Jose Silva <71193150+ShindouAstrea@users.noreply.github.com>
2025-04-22 23:41:20 -0400

    Update Main.component.tsx
    justyfy

--- src/components/Main.component.tsx
+++ src/components/Main.component.tsx
@@ -7,19 +7,19 @@ export default  function Main(){
     }
     return(
         <div  className=' w-full lg:h-screen p-2 ' onContextMenu={handlerContext}>
-            <div className='max-w-[1240px] mx-auto flex flex-col justify-stretch h-full mt-[8rem] text-center'>
-                <div className='flex flex-col text-center items-center alig gap-8'>
+            <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full mt-[8rem] text-justify'>
+                <div className='flex flex-col text-justify items-center alig gap-8'>
                     <h1 className=' text-white text-4xl font-bold uppercase text-center min-[300px]:w-[20rem] px-2'>Acerca de mi</h1>
                    
 
                     <h2 className='text-white text-3xl text-center justify-center items-center uppercase '> Hola !  , soy <span id ="nombre"className='text-[#85c1e9] uppercase'>José Silva </span>un Ingeniero de Software.</h2>
-                    <p className="text-center justify-center text-gray-400 text-2xl lg:w-[80rem] px-1 ">
+                    <p className="text-justify justify-center text-gray-400 text-2xl lg:w-[80rem] px-1 ">
                         Soy ingeniero de ejecución en informática con principal experiencia en el desarrollo web.
                         Me considero alguien adaptable y con habilidades interpersonales sólidas, lo que me permite trabajar bien en equipo y con personas de diferentes
                         orígenes y habilidades.
                        
                         </p>
-                        <p className="text-center justify-center text-gray-400 text-2xl lg:w-[80rem] px-1 ">
+                        <p className="text-justify justify-center text-gray-400 text-2xl lg:w-[80rem] px-1 ">
                         Algunas palabras que me describen son responsabilidad, flexibilidad, altruismo y paciencia.
                         Me apasiona aprender cosas nuevas y trabajar en proyectos que tengan un impacto positivo en la sociedad.
                         Además, tengo habilidades en diferentes FrameWorks de desarrollo web, las cuales he adquirido a través de mi experiencia en  mi práctica profesional , proyectos personales, proyectos acádemicos y proyectos colaborativos. En mi tiempo libre, disfruto de escuchar mis playlist de Spotify, ejercitarme, ver series , jugar algunos juegos y desarrollar algunos proyectos personales.
